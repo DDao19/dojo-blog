8c9c8ead693545845d3d4e19350038622ceb11ec
ddao19 <dung.dao19@outlook.com>
2021-09-13 22:08:43 -0500

    added a form to create new blogs

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleTitleChange = (e) => {
+    let blogTitle = e.target.value;
+    setTitle(blogTitle);
+  };
+
+  const handleTextArea = (e) => {
+    let blogBody = e.target.value;
+    setBody(blogBody);
+  };
+
+  const handleAuthor = (e) => {
+    let blogAuthor = e.target.value;
+    setAuthor(blogAuthor);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+  console.log(author);
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <label>Blog Body</label>
+        <textarea required value={body} onChange={handleTextArea}></textarea>
+        <label>Blog Author</label>
+        <select value={author} onChange={handleAuthor}>
+          <option value="" disabled>
+            --select Author--
+          </option>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
